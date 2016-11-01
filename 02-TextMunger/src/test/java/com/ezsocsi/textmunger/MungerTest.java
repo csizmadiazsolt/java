4885ae179ba8f6ee22c37b15abf8de23bdd806fa
Zsolt Csizmadia <zsolt.csizmadia@ericsson.com>
2016-11-01 11:13:12 +0100

    [All] Sonar fixes merged into this branch because I had some GIT issue and it was the easiest way... ProjectEuler1 also on this branch without unit tests.

--- 02-TextMunger/src/test/java/com/ezsocsi/textmunger/MungerTest.java
+++ 02-TextMunger/src/test/java/com/ezsocsi/textmunger/MungerTest.java
@@ -6,59 +6,47 @@ import com.ezsocsi.utils.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class MungerTest {
     private Munger munger;
 
+    private final List<Character> WORD_FRAGMENTATION = TestUtils.stringToCharList("fragmentation");
+    private final List<Character> WORD_FRAGMENTATION_SHUFFLED = TestUtils.stringToCharList("figmenattraon");
+
+    private final List<Character> WORD_SOME = TestUtils.stringToCharList("some");
+    private final List<Character> WORD_SOME_SHUFFLED = TestUtils.stringToCharList("smoe");
+    private final List<Character> WORD_TEXT = TestUtils.stringToCharList("text");
+    private final List<Character> WORD_TEXT_SHUFFLED = TestUtils.stringToCharList("txet");
+    private final List<Character> WORD_FOR = TestUtils.stringToCharList("for");
+    private final List<Character> WORD_EXAMPLE = TestUtils.stringToCharList("example");
+    private final List<Character> WORD_EXAMPLE_SHUFFLED = TestUtils.stringToCharList("eaxmple");
+
     @Before
     public void setup() {
         ListUtils listUtils = mock(ListUtils.class);
         munger = new Munger(listUtils);
 
-        doCallRealMethod().when(listUtils).shuffle(anyList(), anyInt(), anyInt());
+        doReturn(WORD_FRAGMENTATION_SHUFFLED).when(listUtils).shuffle(WORD_FRAGMENTATION, 1, 12);
+
+        doReturn(WORD_SOME_SHUFFLED).when(listUtils).shuffle(WORD_SOME, 1, 3);
+        doReturn(WORD_TEXT_SHUFFLED).when(listUtils).shuffle(WORD_TEXT, 1, 3);
+        doReturn(WORD_FOR).when(listUtils).shuffle(WORD_FOR, 1, 2);
+        doReturn(WORD_EXAMPLE_SHUFFLED).when(listUtils).shuffle(WORD_EXAMPLE, 1, 6);
     }
 
     @Test
     public void munger_returns_correct_munged_word() {
-        String generatedOutput = munger.munge("granite");
-
-        int letterCounter = generatedOutput.indexOf('g') + generatedOutput.indexOf('r')
-                + generatedOutput.indexOf('a') + generatedOutput.indexOf('n') + generatedOutput.indexOf('i')
-                + generatedOutput.indexOf('t') + generatedOutput.indexOf('e');
-
-        assertEquals(21, letterCounter);
+        String expectedOutput = "figmenattraon";
+        assertEquals(expectedOutput, munger.munge("fragmentation"));
     }
 
     @Test
     public void munger_returns_correct_munged_sentence() {
-        String generatedOutput = munger.munge("Brooklyn Heights, isn't it nice?");
-
-        long numberOf_Bs = TestUtils.countOccurrences(generatedOutput, 'B');
-        long numberOf_rs = TestUtils.countOccurrences(generatedOutput, 'r');
-        long numberOf_os = TestUtils.countOccurrences(generatedOutput, 'o');
-        long numberOf_ks = TestUtils.countOccurrences(generatedOutput, 'k');
-        long numberOf_ls = TestUtils.countOccurrences(generatedOutput, 'l');
-        long numberOf_ys = TestUtils.countOccurrences(generatedOutput, 'y');
-        long numberOf_ns = TestUtils.countOccurrences(generatedOutput, 'n');
-        long numberOf_Spaces = TestUtils.countOccurrences(generatedOutput, ' ');
-        long numberOf_Hs = TestUtils.countOccurrences(generatedOutput, 'H');
-        long numberOf_es = TestUtils.countOccurrences(generatedOutput, 'e');
-        long numberOf_is = TestUtils.countOccurrences(generatedOutput, 'i');
-        long numberOf_gs = TestUtils.countOccurrences(generatedOutput, 'g');
-        long numberOf_hs = TestUtils.countOccurrences(generatedOutput, 'h');
-        long numberOf_ts = TestUtils.countOccurrences(generatedOutput, 't');
-        long numberOf_ss = TestUtils.countOccurrences(generatedOutput, 's');
-        long numberOf_comas = TestUtils.countOccurrences(generatedOutput, ',');
-        long numberOf_Apostrophes = TestUtils.countOccurrences(generatedOutput, '\'');
-        long numberOf_cs = TestUtils.countOccurrences(generatedOutput, 'c');
-        long numberOf_QuestionMarks = TestUtils.countOccurrences(generatedOutput, '?');
-
-        long sum = numberOf_Apostrophes + numberOf_Bs + numberOf_cs + numberOf_comas + numberOf_es + numberOf_gs
-                + numberOf_hs + numberOf_Hs + numberOf_is + numberOf_ks + numberOf_ls + numberOf_ns + numberOf_os
-                + numberOf_rs + numberOf_Spaces + numberOf_ss + numberOf_ts + numberOf_ys + numberOf_QuestionMarks;
-
-        assertEquals(32, sum);
+        String expectedOutput = "smoe txet for eaxmple";
+        assertEquals(expectedOutput, munger.munge("some text for example"));
     }
 }
