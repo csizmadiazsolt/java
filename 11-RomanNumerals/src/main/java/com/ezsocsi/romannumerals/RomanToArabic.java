14b8ebbeff4613e76e26f123297539b690245ded
Zsolt Csizmadia <zsolt.csizmadia@ericsson.com>
2016-12-25 22:04:59 +0100

    [11-RomanNumerals] ArabicToRoman and RomanToArabic converters without tests checks and DI.

---
+++ 11-RomanNumerals/src/main/java/com/ezsocsi/romannumerals/RomanToArabic.java
@@ -0,0 +1,7 @@
+package com.ezsocsi.romannumerals;
+
+/**
+ * Created by ezsocsi on 12/23/2016.
+ */
+public class RomanToArabic {
+}
---
+++ 11-RomanNumerals/src/main/java/com/ezsocsi/romannumerals/RomanToArabicConverter.java
@@ -0,0 +1,7 @@
+package com.ezsocsi.romannumerals;
+
+/**
+ * Created by ezsocsi on 12/24/2016.
+ */
+public class RomanToArabicConverter {
+}
